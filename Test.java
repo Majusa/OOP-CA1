eb572de64b8011158b315ed7987713ba0511cb64
finalMattG <matt.99@ntlworld.com>
2015-02-19 23:24:30 +0000

    Added subtle humour
    Test program now includes ancient meme and academic joke

--- Test.java
+++ Test.java
@@ -1,10 +1,12 @@
 /**
- * Tests Student, Staff, Module and ObjectArrayList
+ * Simple program which tests Student, Staff, Module and (indirectly) ObjectArrayList.
  * @author Matt Gilbert
  */
 public class Test {
 	
 	public static void main(String[] args) {
+	
+		// Test the constructors by making some students, staff and modules
 		Student ben        = new Student("Ben", "Fullalove", 0, "bf254");
 		Student egor       = new Student("Egor", "Grigorev", 1, "gg276");
 		Student matt       = new Student("Matt", "Gilbert", 2, "mdg209");
@@ -18,35 +20,55 @@ public class Test {
 		Module  socProf    = new Module("ECM1407",
 			"Social and Professional Issues of the Information Age", 15);
 		
+		// Set up staff for some Term 1 modules
 		yiming.appendModuleCoordinate(socProf);
 		yiming.appendModuleLecture(socProf);
-		zena.appendModuleCoordinate(socProf);
-		zena.appendModuleLecture(socProf);
-		forScience.addCoordinator(richard);
-		forScience.addLecturer(richard);
+		zena.appendModuleCoordinate(socProf); // Add coordinator by staff
+		zena.appendModuleLecture(socProf);    // Add lecturer by staff
+		forScience.addCoordinator(richard);   // Add coordinator by module
+		forScience.addLecturer(richard);      // Add lecturer by module
 		
-		matt.enroll(socProf);
-		forScience.addStudent(matt);
-		assert matt.getCredits() > 0;
+		// Enroll Matt on some Term 1 modules
+		matt.enroll(socProf);                 // Add student by student
+		forScience.addStudent(matt);          // Add student by module
 		
-		socProf.remCoordinator(yiming);
-		socProf.remLecturer(yiming);
-		zena.removeModuleCoordinate(socProf);
-		zena.removeModuleLecture(socProf);
+		// Remove Zena and Yiming
+		socProf.remCoordinator(yiming);       // Remove coordinator by module
+		socProf.remLecturer(yiming);          // Remove lecturer by module
+		zena.removeModuleCoordinate(socProf); // Remove coordinator by staff
+		zena.removeModuleLecture(socProf);    // Remove lecturer by staff
 		
-		matt.unEnroll(forScience);
-		socProf.remStudent(matt);
+		// Unenroll Matt from Term 1 modules
+		matt.unEnroll(forScience);            // Remove student by student
+		socProf.remStudent(matt);             // Remove student by module
 		
+		// Set up staff and students for a Term 2 module
 		oop.addCoordinator(jonathan);
 		jonathan.appendModuleLecture(oop);
-		
 		ben.enroll(oop);
 		egor.enroll(oop);
 		oop.addStudent(matt);
 		oop.addStudent(tom);
-		assert ben.getCredits() == egor.getCredits();
 		
+		// Check getCredits(), numberOfLectureModules() and numberOfCoordinateModules()
+		assert ben.getCredits() == egor.getCredits();
 		assert jonathan.numberOfLectureModules() > yiming.numberOfLectureModules();
 		assert richard.numberOfCoordinateModules() > zena.numberOfCoordinateModules();
+		
+		// Can we enroll Matt on to a module which would increase his credits past 120?
+		int someCredits = matt.getCredits();
+		assert someCredits > 0;
+		Module allYourCredits("AYC9001", "All your credits are belong to us", 120);
+		assert !matt.enroll(allYourCredits);       // Enroll should fail
+		assert !(matt.getCredits() > someCredits); // Matt should not get more credits
+		
+		// Can we remove staff and students from modules they are not part of?
+		Module weaving = ("UBW1024", "Underwater Basket Weaving", -15);
+		assert !egor.unEnroll(weaving);
+		assert !jonathan.removeModuleLecture(weaving);
+		assert !ricahrd.removeModuleCoordinate(weaving);
+		assert !weaving.remStudent(ben);
+		assert !weaving.remLecturer(zena);
+		assert !weaving.remCoordinator(yiming);
 	}
 }
