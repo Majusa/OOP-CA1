0a0a54e6bcd130ff2ce32e0cf8bbe3c9664aab06
Ben F <benfullalove123@gmail.com>
2015-02-20 08:36:21 +0000

    Update Staff.java
    Added comments and checks for duplicates when adding to list.

--- Staff.java
+++ Staff.java
@@ -1,5 +1,12 @@
-public class Staff {
-
+/**
+ * The Staff class allows Staff to append and remove lecture and coordinated modules
+ * as well as storing personal information and identification of the Staff.
+ * 
+ * @author (Ben Fullalove)
+ */
+ public class Staff {
+ 	
+	//Declare attributes for Staff
 	String forename;
 	String surname;
 	long id;
@@ -7,6 +14,7 @@ public class Staff {
 	ObjectArrayList lectureModules;
 	ObjectArrayList coordinateModules;
 	
+	//Constructor which inititialises or assigns all attributes
 	public Staff(String forename, String surname, long id, String address) {
 		this.forename=forename;
 		this.surname=surname;
@@ -16,8 +24,9 @@ public class Staff {
 		coordinateModules = new ObjectArrayList();
 	}
 	
+	//Functions used to handle lectured modules
 	public void appendModuleLecture(Module newModule) {
-		if !(this.lectureModules.contains(existingModule)) {
+		if !(this.lectureModules.contains(newModule)) {
 			lectureModules.add(newModule);
 		}
 	}
@@ -32,8 +41,9 @@ public class Staff {
 		}
 	}
 	
+	//Functions used to handle coordinated modules
 	public void appendModuleCoordinate(Module newModule) {
-		if !(this.lectureModules.contains(existingModule)) {
+		if !(this.lectureModules.contains(newModule)) {
 			coordinateModules.add(newModule);
 		}
 	}
@@ -48,6 +58,7 @@ public class Staff {
 		}
 	}
 	
+	//Functions to return the size of module lists
 	public int numberOfLectureModules() {
 		return lectureModules.size();
 	}
@@ -56,12 +67,17 @@ public class Staff {
 		return coordinateModules.size();
 	}
 	
+	//Function to check if the given Staff and this Staff have all ettributes equal
 	public boolean equals(Object object) {
 		if !(object instanceof Staff) {
+			//need to return false here else we cannot cast it to a Staff
 			return false;
 		}
 		else {
+			//cast the object into a Staff
 			Staff staff = (Staff) object;
+			
+			//check all attributes of the two Staff are equal
 			if (staff.forename.equals(forename) && staff.surname.equals(surname) && staff.id==id && staff.address.equals(address) && staff.lectureModules.equals(lectureModules) && staff.coordinateModules.equals(coordinateModules)) {
 				return true;	
 			}
